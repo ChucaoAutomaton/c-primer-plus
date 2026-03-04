fd58404fbb8faf664eac4166945506cdc9b880bb
ChucaoAutomaton <Chucao_Automaton@outlook.com>
2026-03-04 19:07:24 +0800

    测试'\r'和'\b'

---
+++ test_escape.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <inttypes.h>
+int main() {
+    double salary;
+    printf("$_____\b\b\b\b\b");
+    scanf("%"PRId64, &salary);
+    printf("a\rbaibai");
+    return 0;
+}
