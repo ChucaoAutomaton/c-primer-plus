36da767bcd32fd363c0e2770ee1e0951eb347884
ChucaoAutomaton <Chucao_Automaton@outlook.com>
2026-03-08 21:52:08 +0800

    在宏中加入泛型选择和函数指针

--- test_generic_programming.c
+++ test_generic_programming.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<ctype.h>
+#include<math.h>
 
 #define MAP(x) _Generic(x, double : 0, int : 1)
+#define SQRT(x) _Generic(x, long double : sqrtl, float : sqrtf, default : sqrt)(x)
 
 void showDouble() {
     puts("it is double!");
@@ -20,4 +22,5 @@ int main() {
     arr[1] = showIntergrater;
     arr[MAP(0)](0);
     arr[MAP(1.0)](1.0);
+    printf("%llf\n", SQRT(1.0));
 }
